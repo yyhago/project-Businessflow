7d60141351ef66b7128ca07a02e1befd907d0369
Yhago Felipe <yhago.felipe.teles@gmail.com>
2023-11-29 12:56:58 -0300

    Connect and create API

--- src/Components/Project/ProjectForm.js
+++ src/Components/Project/ProjectForm.js
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
+
 import styles from "./ProjectForm.module.css";
 import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 
 export default function ProjectForm({btnTest}) {
+
+  const [categoris, setCategoris] = useState([])
+
+   useEffect(() => {
+    fetch('http://localhost:5000/categoris', {      // REQUISIÇÃO DA MINHA API (CHAMADA DA MESMA)
+    method:'GET',
+    headers: {
+      'Content-Type':'application/json'
+    },
+  })
+  .then((resp) => resp.json())
+  .then((data) => {
+    setCategoris(data)
+  })
+  .catch((err) => console.log(err))
+   }, [])
+
+
   return (
     <form className={styles.formContainer}>
       <Input
@@ -22,6 +42,7 @@ export default function ProjectForm({btnTest}) {
       <Select
         name="idCategoria"
         text="Selecione a categoria"
+        options={categoris}
       />
 
       <SubmitButton text={btnTest}/>
