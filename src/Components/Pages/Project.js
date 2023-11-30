e2bcfe5f782a88dbe23dbc094977295141cf3085
Yhago Felipe <yhago.felipe.teles@gmail.com>
2023-11-30 16:11:22 -0300

    View project's an real time

--- src/Components/Pages/Project.js
+++ src/Components/Pages/Project.js
@@ -1,18 +1,61 @@
-import Message from "../Layout/Message";
 import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Message from "../Layout/Message";
+import Container from "../Layout/Container";
+import LinkButton from "../Layout/LinkButton";
+import ProjectCard from "../Project/ProjectCard";
+
+import styles from "./Project.module.css";
 
 export default function Project() {
+  const [projects, setProjects] = useState([]);
 
-    const location = useLocation()
-    let message = ''
-    if (location.state){
-        message = location.state.message
-    }
+  const location = useLocation();
+  let message = "";
+  if (location.state) {
+    message = location.state.message;
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:5000/projects", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data)
+        setProjects(data)
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
-    <div>
-         {message && <Message type="sucesso" msg={message} />}
-      <h2>Seus projeto</h2>
+    <div className={styles.projectContainer}>
+      <div className={styles.titleContainer}>
+        <h1>Meus Projetos</h1>
+        <LinkButton
+          to="/newproject"
+          text="Criar Novo Projeto"
+        />
+      </div>
+      {message && (
+        <Message
+          type="sucesso"
+          msg={message}
+        />
+      )}
+      <Container customClass="start">
+        {projects.length > 0 &&
+          projects.map((project) => <ProjectCard
+          id={project.id}
+          name={project.name}
+          valorTotal={project.valorTotal}
+          category={project.category ? project.category.name : 'Sem categoria'}
+          key={project.id}
+          />)}
+      </Container>
     </div>
   );
 }
---
+++ src/Components/Project/ProjectCard.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom"
+import styles from "./ProjectCard.module.css"
+import { bsPencil, BsFillTrashFill, BsPencil, BsPen } from "react-icons/bs"
+
+export default function ProjectCard ({ id, name, category, valorTotal, handleRemove }) {
+    return(
+        <div className={styles.cardProject}>
+            <h2>{name}</h2>
+            <p><span>Orçamento Total:</span> R$: {valorTotal}</p>
+            <p className={styles.categoryText}>
+            <span className={`${styles[category.toLowerCase()]}`}></span>Categoria: {category}</p>
+            <div className={styles.projectCardAction}>
+                <Link to="/">
+                    <BsPencil/> Editar
+                </Link>
+                <button>
+                    <BsFillTrashFill/> Excluir
+                </button>
+            </div>
+        </div>
+    )
+}
